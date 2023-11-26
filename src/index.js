62e24584eac3137ca94e02b8d11c4b1a9806f952
Brent Ross Habawel <rosshabawel12@gmail.com>
2023-11-26 14:51:54 +0800

    used useEffect for movie api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 
 import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} />
+    <App />
+    {/* <StarRating
+      maxRating={5}
+      message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    /> */}
+    {/* <StarRating size={24} className="test" defaultRating={3} /> */}
   </React.StrictMode>
 );
